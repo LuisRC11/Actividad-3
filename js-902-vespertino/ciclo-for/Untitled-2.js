b359d053cc8aaf15b936c4cda3bbcc2ce38579d2
LuisRC11 <luis.reyes2122@gmail.com>
2024-07-25 22:39:24 -0600

    Delete js-902-vespertino directory

--- js-902-vespertino/ciclo-for/Untitled-2.js
+++
@@ -1,5 +0,0 @@
-
-for(let i =1; i<11;i++){
-    num=5
-    console.log(num + "*"+i+ "=" +num*i  );
-}
--- js-902-vespertino/ciclo-for/Untitled-3.js
+++
@@ -1,7 +0,0 @@
-
-for(let n =1; n<11;n++){
-    for(let i =1; i<11;i++){
-     
-        console.log(n + "*"+i+ "=" +n*i  );
-    }  
-}
